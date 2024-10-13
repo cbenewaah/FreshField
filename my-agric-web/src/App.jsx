800cbcdb6f4924218db498a00630256bbe6706be
cbenewaah <benewaahchristabel@gmail.com>
2024-10-13 17:43:16 +0000

    My agriculture website

---
+++ my-agric-web/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import React from 'react'
+import Navbar from './Components/Navbar';
+import Main from './Pages/Main';
+import Footer from './Components/Footer';
+
+
+function App() {
+  return (
+    <div>
+        <Navbar/>
+        <Main/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
